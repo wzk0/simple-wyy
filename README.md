b7aef9a9149cf86596d32794a7e194abbc25a706
wzk0 <729301277@qq.com>
2022-10-19 21:57:18 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # simple-wyy
 一个功能和界面都简单的网易云前后端
+
+## 效果
+
+![首页](https://ghproxy.com/https://raw.githubusercontent.com/wzk0/photo/main/202210192122486.png)
+
+![搜索](https://ghproxy.com/https://raw.githubusercontent.com/wzk0/photo/main/202210192122895.png)
+
+![搜索结果](https://ghproxy.com/https://raw.githubusercontent.com/wzk0/photo/main/202210192123863.png)
+
+![热搜](https://ghproxy.com/https://raw.githubusercontent.com/wzk0/photo/main/202210192123841.png)
+
+![单页](https://ghproxy.com/https://raw.githubusercontent.com/wzk0/photo/main/202210192129789.png)
+
+> 单页的歌词有点丑了...
+
+## 部署
+
+### 服务器端
+
+正在研究中...
+
+### 本地
+
+在确保电脑上有git的情况下执行如下指令:
+
+```
+git clone https://github.com/wzk0/simple-wyy && cd simple-wyy
+pip install -r requirements.txt
+export FLASK_APP=start
+flask run
+```
+
+## 开发
+
+使用到了网易云API - https://github.com/Binaryify/NeteaseCloudMusicApi
+
+单页在线播放器为APlayer - https://aplayer.js.org/#/zh-Hans/
---
+++ start.py
@@ -0,0 +1,77 @@
+from flask import Flask,render_template,request,redirect,url_for
+import requests
+import json
+
+app = Flask(__name__,template_folder='./static/templates')
+api='https://wyyapi-wzk0.vercel.app'
+
+def analyze(dic):
+    ls=[]
+    for l in dic['result']['songs']:
+        ar=[]
+        for ll in l['ar']:
+            ar.append(ll['name'])
+        d={'title':l['name']+' - '+','.join(ar),'year':str(l['id'])}
+        ls.append(d)
+    return ls
+
+@app.route('/')
+def hello():
+    return render_template('index.html',name='Thdbd')
+
+@app.route('/hot')
+def hot():
+    global api
+    hot='/search/hot/detail'
+    r=requests.get(api+hot).text
+    ls=json.loads(r)['data']
+    ll=[]
+    for l in ls:
+        ll.append({'title': l['searchWord'], 'year': l['score']})
+    return render_template('hot.html',name='Thdbd',movies=ll)
+
+@app.route('/search',methods=['GET','POST'])
+def ss():
+    global api
+    if request.method == 'POST':
+        title=request.form.get('title')
+        year=request.form.get('year')
+        ss='/cloudsearch'
+        params={'keywords':title,'limit':year}
+        r=requests.get(api+ss,params=params)
+        return redirect(url_for('res',movies=r.url))
+    return render_template('search.html')
+
+@app.route('/result/<path:movies>')
+def res(movies):
+    r=requests.get(movies)
+    movies=analyze(json.loads(r.text))
+    return render_template('result.html',movies=movies)
+
+@app.route('/download',methods=['GET','POST'])
+def download():
+    global api
+    if request.method == 'POST':
+        title=request.form.get('title')
+        return redirect(url_for('dl',uid=title))
+    return render_template('download.html')
+
+@app.route('/dl/<int:uid>')
+def dl(uid):
+    global api
+    dl='/song/url/v1'
+    params={'id':uid,'level':'higher'}
+    url=json.loads(requests.get(api+dl,params=params).text)['data'][0]['url']
+    lrc=json.loads(requests.get(api+'/lyric?id='+str(uid)).text)['lrc']['lyric']
+    movies=json.loads(requests.get(api+'/song/detail?ids='+str(uid)).text)
+    name=movies['songs'][0]['name']
+    ar=[]
+    for a in movies['songs'][0]['ar']:
+        ar.append(a['name'])
+    pic=movies['songs'][0]['al']['picUrl']
+    namels=[name,','.join(ar),pic]
+    return render_template('play.html',url=url,lrc=lrc,namels=namels)
+
+@app.errorhandler(404)
+def pnf(e):
+    return render_template('404.html'),404
